67783eeff3c4f2fc270b3541a46732e55580b087
Silvio Martinec <silvio.martinec.ie@gmail.com>
2025-01-02 15:43:29 +0000

    Add db tables and Admin creds

--- neurographic/settings.py
+++ neurographic/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = 'neurographic.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
