8cdfe9b22c589f62093c95f1dc049cd6cdbb92e4
Silvio Martinec <silvio.martinec.ie@gmail.com>
2025-01-14 17:41:59 +0000

    Add new imports and view function with template

--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 # from django.http import HttpResponse  DEL
+from django.views import generic
+from .models import Post
 
 # Create your views here.
 
 # def journal(request):   DEL
 #     return HttpResponse("This is Journal page")
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    template_name = "post_list.html"
