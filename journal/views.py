765d2160c50650601397b81d51911028ae91a8ab
Silvio Martinec <silvio.martinec.ie@gmail.com>
2025-01-20 18:40:11 +0000

    Add new urls.py pa, update anchor, function and imamage for full post view

--- journal/views.py
+++ journal/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -6,6 +6,28 @@ from .models import Post
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created")
-    # template_name = "post_list.html"  DELETE <-
     template_name = "journal/index.html"
     paginate_by = 6
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`journal.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`journal.Post`.
+
+    **Template:**
+
+    :template:`journal/post_detail.html`
+    """
+
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "journal/post_detail.html",
+        {"post": post},
+    )
