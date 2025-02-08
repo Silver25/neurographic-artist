5c0cfd08a10e9581507e3df4313b0a99b15a1fae
Silver25 <silvio.martinec.ie@gmail.com>
2025-02-08 16:08:45 +0000

    Created new view for the modification of the comment

--- journal/views.py
+++ journal/views.py
@@ -48,6 +48,9 @@ def post_detail(request, slug):
     comment_form = CommentForm()
 
     # helper function
+    # context ("post") is how we pass data from our views to our templates
+    # context is a Python dictionary of key/value pairs that is sent to the template
+    # It is convention that the key name would be the same as the variable name
     return render(
         request,
         "journal/post_detail.html",  # path to the template file
@@ -58,6 +61,27 @@ def post_detail(request, slug):
             "comment_form": comment_form,
          }
     )
-    # context ("post") is how we pass data from our views to our templates
-    # context is a Python dictionary of key/value pairs that is sent to the template
-    # It is convention that the key name would be the same as the variable name
+
+# view returns editor to the post webpage after 
+# mofification/edit of the comment is finished
+def comment_edit(request, slug, comment_id):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
