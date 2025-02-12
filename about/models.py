892ab344bd19ceda41ee25a6ec75d3c0a8c677f8
Silver25 <silvio.martinec.ie@gmail.com>
2025-02-12 12:27:53 +0000

    Add code for the default post image from statis and featured from cloudinary

--- about/models.py
+++ about/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+# If a new model field is added, must run 'makemigrations' to create
+# a new migrations file, and 'migrate' to write changes to database
 class About(models.Model):
     title = models.CharField(max_length=200)
     updated = models.DateTimeField(auto_now=True)
--- journal/models.py
+++ journal/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))  # constant
 
 # Create your models here.
+# If a new model field is added, must run 'makemigrations' to create
+# a new migrations file, and 'migrate' to write changes to database
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)  # must be unique
     slug = models.SlugField(max_length=200, unique=True)  # must be unique
@@ -12,6 +15,7 @@ class Post(models.Model):
         on_delete=models.CASCADE,
         related_name="journal_posts"
 )
+    featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
